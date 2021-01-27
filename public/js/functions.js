7c0573681bce5532ac2fea7a2388441a40ad6130
saida <ssubelza23@gmail.com>
2021-01-27 19:15:47 -0300

    correcciones

--- public/js/functions.js
+++ public/js/functions.js
@@ -1,6 +1,35 @@
-($('#staticBackdrop').on('shown.bs.modal', function () {
-    $('#myInput').trigger()
-  })
-)
+
+(function() {
+  'use strict';
+  window.addEventListener('load', function() {
+    // Fetch all the forms we want to apply custom Bootstrap validation styles to
+    var forms = document.getElementsByClassName('needs-validation');
+    // Loop over them and prevent submission
+    var validation = Array.prototype.filter.call(forms, function(form) {
+      form.addEventListener('submit', function(event) {
+        if (form.checkValidity() === false) {
+          event.preventDefault();
+          event.stopPropagation();
+        }
+        form.classList.add('was-validated');
+      }, false);
+    });
+
+    var triggerTabList = [].slice.call(document.querySelectorAll('#nav-tab a'))
+    triggerTabList.forEach(function (triggerEl) {
+      var tabTrigger = new bootstrap.Tab(triggerEl)
+    
+      triggerEl.addEventListener('click', function (event) {
+        event.preventDefault()
+        tabTrigger.show()
+      })
+    })
+    var dropdownElementList = [].slice.call(document.querySelectorAll('.dropdown-toggle'))
+var dropdownList = dropdownElementList.map(function (dropdownToggleEl) {
+return new bootstrap.Dropdown(dropdownToggleEl)
+})
+    
+  }, false);
+})();
 
 
